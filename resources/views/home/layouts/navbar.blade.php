339291097f5b59ded930a2a83bbf2c775fe46a18
Easbi Ikhsan <easterikhxen@gmail.com>
2024-01-23 15:16:32 +0700

    manajemen link bisa di klik

--- resources/views/home/layouts/navbar.blade.php
+++ resources/views/home/layouts/navbar.blade.php
@@ -32,7 +32,7 @@
                     </ul>
                 </li>
                 <li><a class="nav-link scrollto" href="#contact">Contact</a></li> --}}
-                <li><a class="getstarted scrollto" href="/dashboard">MANAJEMEN LINK</a></li>
+                <li><a class="getstarted scrollto" href="{{ url('/dashboard')}}">MANAJEMEN LINK</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
